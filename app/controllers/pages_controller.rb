112e62666da436be60e65e436c2e685357269025
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-06-09 12:29:04 +0200

    pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+  end
+
+  def capabilities
+  end
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'pages#home'
+get 'about', to: 'pages#about'
+get 'contact', to: 'pages#contact'
+get 'capabilities', to: 'pages#capabilities'
 end
