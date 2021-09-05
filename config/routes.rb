ee0914ceb9e7d6ebbcbf31d8f3e6f07cb327ac8b
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-09-05 21:26:28 +0200

    home

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ root to: 'pages#home'
 get 'about', to: 'pages#about'
 get 'contact', to: 'pages#contact'
 get 'capabilities', to: 'pages#capabilities'
-get '/capabilities/readmore', to: 'pages#readmore'
+
 end
